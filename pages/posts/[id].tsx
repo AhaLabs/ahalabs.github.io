9a54fdd4116d907179021a3df1491c22f209f36a
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-03-28 12:16:59 -0400

    feat: add syntax highlighting

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,9 +2,10 @@ import Layout from "../../components/layout";
 import Date from "../../components/date";
 import { getPosts, getPost } from "../../lib/posts";
 import type { Post } from "../../lib/posts";
-import Head from "next/head";
 import utilStyles from "../../styles/utils.module.css";
 import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark as dark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 export async function getStaticPaths() {
   const posts = getPosts();
@@ -32,7 +33,27 @@ export default function Post(post: Post) {
           <Date dateString={post.date} />
         </div>
       </article>
-      <Markdown children={post.markdown} />
+      <Markdown
+        children={post.markdown}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={dark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      />
     </Layout>
   );
 }
