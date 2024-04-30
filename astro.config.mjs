c3cddf0816aabbda3b5aacf50f2e8c366603aa99
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-04-29 21:47:12 -0400

    add org chart lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import { addLayoutToBlogPosts, remarkReadingTime } from './markdown-extensions.mjs';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import { mermaid } from "./src/plugins/mermaid";
+import {
+  addLayoutToBlogPosts,
+  remarkReadingTime,
+} from "./markdown-extensions.mjs";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ahalabs.dev',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [addLayoutToBlogPosts, remarkReadingTime],
-		extendDefaultPlugins: true,
-	}
+  site: "https://ahalabs.dev",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    remarkPlugins: [addLayoutToBlogPosts, remarkReadingTime, mermaid],
+    extendDefaultPlugins: true,
+  },
 });
---
+++ src/pages/org-chart.md
@@ -0,0 +1,13 @@
+---
+layout: ../layouts/Letterhead.astro
+---
+
+# Org Chart
+
+```mermaid
+graph TD
+  A["Chad & Willem (Aha Labs) + Enzo Soyer (Limus Vita)"] -->|employ| B[Elizabeth Engelman]
+  A -.->|contract| C[Blaine Heffron]
+  A -.->|contract| D[Audrey Sonntag]
+  A -.->|contract| E[Greg Farrenkopf]
+```
