baa595e17e54104915edecc1e0e436d3b7d1d66e
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-04-29 21:53:55 -0400

    update site description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Aha Labs';
-export const SITE_DESCRIPTION = 'Makers of RAEN. Aha moments for NEAR, blockchain, and open source.';
+export const SITE_TITLE = "Aha Labs";
+export const SITE_DESCRIPTION =
+  "A developer experience consultancy focused on delightful experiences for Stellar.";
